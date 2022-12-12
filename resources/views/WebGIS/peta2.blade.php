1ab1de81c3e5e39e537d6b0bb3abaa42aee738d2
beni07if <beni07if@gmail.com>
2022-12-12 16:55:37 +0700

    update logo

--- resources/views/WebGIS/peta2.blade.php
+++ resources/views/WebGIS/peta2.blade.php
@@ -6,7 +6,7 @@
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
         <a href="{{ route('index') }}" class="logo d-flex align-items-center">
-            <img src="assets/img/logo/logo-webgis-pupr-blue.png" alt="">
+            <img src="assets/img/logo/logo-web-bangunan2.png" alt="">
             <!-- <span>WebGIS PS Ketapang</span> -->
         </a>
 
