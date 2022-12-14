efc50328e807257f155a757d30e5bd57262cfc49
beni07if <beni07if@gmail.com>
2022-12-14 09:57:55 +0700

    update title

--- resources/views/WebGIS/Layouts/HeadStyle.blade.php
+++ resources/views/WebGIS/Layouts/HeadStyle.blade.php
@@ -1,7 +1,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>WebGIS Jalan PUPR Pontianak</title>
+    <title>Database Bangunan Gedung PUPR Pontianak</title>
     <meta content="" name="description">
 
     <meta content="" name="keywords">
